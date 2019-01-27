addfbb6811139c789c380cd81e3206b3fc89c70b
Stepan <kurakste@gmail.com>
2019-01-27 19:50:00 +0300

    debuging

--- examples/bot-plus.php
+++ examples/bot-plus.php
@@ -74,19 +74,27 @@ try {
                             ->setButtons([
                                 (new \Viber\Api\Keyboard\Button())
                                     ->setBgColor('#8074d6')
-                                    ->setColumns(2)
+                                    ->setColumns(3)
                                     ->setTextSize('small')
                                     ->setTextHAlign('right')
                                     ->setActionType('reply')
-                                    ->setActionBody('btn-click')
-                                    ->setText('Button 1'),
+                                    ->setActionBody('menu-sub-1')
+                                    ->setText('subMenu 1'),
 
                                 (new \Viber\Api\Keyboard\Button())
                                     ->setBgColor('#2fa4e7')
+                                    ->setColumns(3)
+                                    ->setTextHAlign('center')
+                                    ->setActionType('reply')
+                                    ->setActionBody('menu-sub-2')
+                                    ->setText('subMenu 2'),
+                                (new \Viber\Api\Keyboard\Button())
+                                    ->setBgColor('#2fa4e7')
+                                    ->setColumns(6)
                                     ->setTextHAlign('center')
                                     ->setActionType('reply')
                                     ->setActionBody('btn-click')
-                                    ->setText('Button 2'),
+                                    ->setText('To main menu'),
                             ])
                     )
             );
