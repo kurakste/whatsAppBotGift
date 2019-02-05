0848a276a278634987f1ba7616a71c21aa4142e0
Stepan <kurakste@gmail.com>
2019-02-05 20:57:04 +0300

    debuging

--- examples/keyboards/testKeyboard.php
+++ examples/keyboards/testKeyboard.php
@@ -1,27 +1,26 @@
 <?php
-    $kb =
-                        (new \Viber\Api\Keyboard())
-                            ->setButtons([
-                                (new \Viber\Api\Keyboard\Button())
-                                    ->setBgColor('#8074d6')
-                                    ->setTextSize('small')
-                                    ->setTextHAlign('right')
-                                    ->setActionType('reply')
-                                    ->setActionBody('btn-click')
-                                    ->setText('Button 1'),
+    $kb = new \Viber\Api\Keyboard();
+    $kb->setButtons([
+        (new \Viber\Api\Keyboard\Button())
+            ->setBgColor('#8074d6')
+            ->setTextSize('small')
+            ->setTextHAlign('right')
+            ->setActionType('reply')
+            ->setActionBody('btn-click')
+            ->setText('Button 1'),
 
-                                (new \Viber\Api\Keyboard\Button())
-                                    ->setBgColor('#2fa4e7')
-                                    ->setTextHAlign('center')
-                                    ->setActionType('reply')
-                                    ->setActionBody('btn-click')
-                                    ->setText('Button 2'),
+        (new \Viber\Api\Keyboard\Button())
+            ->setBgColor('#2fa4e7')
+            ->setTextHAlign('center')
+            ->setActionType('reply')
+            ->setActionBody('btn-click')
+            ->setText('Button 2'),
 
-                                (new \Viber\Api\Keyboard\Button())
-                                    ->setBgColor('#555555')
-                                    ->setTextSize('large')
-                                    ->setTextHAlign('left')
-                                    ->setActionType('reply')
-                                    ->setActionBody('btn-click')
-                                    ->setText('Button 3'),
-                            ]);
+        (new \Viber\Api\Keyboard\Button())
+            ->setBgColor('#555555')
+            ->setTextSize('large')
+            ->setTextHAlign('left')
+            ->setActionType('reply')
+            ->setActionBody('btn-click')
+            ->setText('Button 3'),
+    ]);
