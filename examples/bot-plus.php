724ca3c407763a585e264b8f8253b68a0b32b5f8
Stepan <kurakste@gmail.com>
2019-02-05 22:13:56 +0300

    debuging

--- examples/bot-plus.php
+++ examples/bot-plus.php
@@ -80,7 +80,6 @@ try {
         })
         ->onText('|menu|s', function ($event) use ($bot, $botSender, $log) {
             $log->info('menu method:');
-            $log->info('btns:'.var_expor($btns));
             $bot->getClient()->sendMessage(
                 (new \Viber\Api\Message\Text())
                     ->setSender($botSender)
