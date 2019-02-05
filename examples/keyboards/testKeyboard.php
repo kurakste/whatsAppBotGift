e13590d8e16aec26da819732b59b7c74ced86ef6
Stepan <kurakste@gmail.com>
2019-02-05 20:55:36 +0300

    debuging

--- examples/keyboards/testKeyboard.php
+++ examples/keyboards/testKeyboard.php
@@ -1,30 +1,27 @@
 <?php
+    $kb =
+                        (new \Viber\Api\Keyboard())
+                            ->setButtons([
+                                (new \Viber\Api\Keyboard\Button())
+                                    ->setBgColor('#8074d6')
+                                    ->setTextSize('small')
+                                    ->setTextHAlign('right')
+                                    ->setActionType('reply')
+                                    ->setActionBody('btn-click')
+                                    ->setText('Button 1'),
 
-$tkb  = (new \Viber\Api\Keyboard())
-        ->setButtons(
-            [
-                (new \Viber\Api\Keyboard\Button())
-                    ->setBgColor('#8074d6')
-                    ->setColumns(3)
-                    ->setTextSize('small')
-                    ->setTextHAlign('right')
-                    ->setActionType('reply')
-                    ->setActionBody('menu-sub-1')
-                    ->setText('subMenu 1'),
+                                (new \Viber\Api\Keyboard\Button())
+                                    ->setBgColor('#2fa4e7')
+                                    ->setTextHAlign('center')
+                                    ->setActionType('reply')
+                                    ->setActionBody('btn-click')
+                                    ->setText('Button 2'),
 
-                (new \Viber\Api\Keyboard\Button())
-                    ->setBgColor('#2fa4e7')
-                    ->setColumns(3)
-                    ->setTextHAlign('center')
-                    ->setActionType('reply')
-                    ->setActionBody('menu-sub-2')
-                    ->setText('subMenu 2'),
-                (new \Viber\Api\Keyboard\Button())
-                    ->setBgColor('#2fa4e7')
-                    ->setColumns(6)
-                    ->setTextHAlign('center')
-                    ->setActionType('reply')
-                    ->setActionBody('btn-click')
-                    ->setText('To main menu'),
-            ]
-        );
+                                (new \Viber\Api\Keyboard\Button())
+                                    ->setBgColor('#555555')
+                                    ->setTextSize('large')
+                                    ->setTextHAlign('left')
+                                    ->setActionType('reply')
+                                    ->setActionBody('btn-click')
+                                    ->setText('Button 3'),
+                            ]);
