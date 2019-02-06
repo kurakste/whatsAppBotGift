cb5a9c9ca8ee32851c45935823f91f53803b1267
Stepan <kurakste@gmail.com>
2019-02-06 12:52:50 +0300

    add menu

--- examples/keyboards/mainMenu.php
+++ examples/keyboards/mainMenu.php
@@ -10,7 +10,6 @@ return
                 ->setActionType('reply')
                 ->setActionBody('usecases')
                 ->setText('Варианты использования'),
-
             (new \Viber\Api\Keyboard\Button())
                 ->setBgColor('#8074d6')
                 ->setColumns(3)
@@ -18,7 +17,21 @@ return
                 ->setActionType('reply')
                 ->setActionBody('benefits')
                 ->setText('Выгоды'),
-
+            (new \Viber\Api\Keyboard\Button())
+                ->setBgColor('#8074d6')
+                ->setColumns(3)
+                ->setTextSize('small')
+                ->setTextHAlign('center')
+                ->setActionType('reply')
+                ->setActionBody('connectors')
+                ->setText('Как это связать с нашими системами?'),
+            (new \Viber\Api\Keyboard\Button())
+                ->setBgColor('#8074d6')
+                ->setColumns(3)
+                ->setTextHAlign('center')
+                ->setActionType('reply')
+                ->setActionBody('effectually')
+                ->setText('Когда есть смысл внедрять?'),
             (new \Viber\Api\Keyboard\Button())
                 ->setBgColor('#2fa4e7')
                 ->setTextSize('large')
