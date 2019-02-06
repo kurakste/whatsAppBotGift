ab584eb946d516a355767097f2f385b59b5e00e9
Stepan <kurakste@gmail.com>
2019-02-06 22:39:14 +0300

    debuging

--- examples/bot-plus.php
+++ examples/bot-plus.php
@@ -165,8 +165,8 @@ try {
         ->onText('|.*|s', function ($event) use ($bot, $botSender, $log) {
             $log->info('onText ' . $event->getMessage()->getText());
             $str = "К сожалению я вас не понимаю. Давайте я вам анекдот расскажу: \n";
-            $joke = require_once(__DIR__.'/skills/humor/gethummor.php');
-            $str = $str.$joke;
+            //$joke = require_once(__DIR__.'/skills/humor/gethummor.php');
+            //$str = $str.$joke;
             // .* - match any symbols
             $bot->getClient()->sendMessage(
                 (new \Viber\Api\Message\Text())
